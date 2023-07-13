d98627658123747c09205d46a728ffbe2e6cf48e
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-07-13 16:01:08 -0500

    Setup environment variables for server

--- api/server.py
+++ api/server.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from fastapi import FastAPI, FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 import boto3
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Initializing boto3 client
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
 
 @app.get("/")
 async def root():
@@ -13,8 +36,13 @@ async def get_image():
     return {""}
 
 @app.post("/image")
-async def upload_image():
-    return {""}
+async def upload_image(file: UploadFile = File(...)):
+    status = True
+
+    return JSONResponse({
+        "success": status,
+        "url": "url"
+    })
 
 @app.delete("/image/{image_id}")
 async def delete_image():
